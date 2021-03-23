a1a54997bd442abb4d523273798d6ef7964daff3
bahadirkesgin <bahadirkesgin@github.com>
2021-03-23 23:44:21 +0300

    Update rigel_lite.py

--- rigel_lite.py
+++ rigel_lite.py
@@ -8,9 +8,10 @@ import wikipedia
 import random
 import os
 import time
+import wolframalpha
+import requests
 from selenium import webdriver
 from nltk.tokenize import sent_tokenize
-import spotify
 
 re = sr.Recogniser()
 mic = sr.Microphone()
@@ -48,24 +49,44 @@ def listen():
         
         if text==0:
             continue
-            
-        if "dur" in str(text) or "çıkış" in str(text) or "görüşürüz" in str(text):
+        
+        if "durdur" in str(text):
             break
-            
-        if 'wikipedia' in text:
+        
+        if "çıkış" in str(text) or "görüşürüz" in str(text):
+            print("Görüşmek üzere!")
+            speak("Görüşmek üzere")
+            break
+        
+        elif "tarih" in str(text):
+            date = datetime.date.today()
+            date = date.strftime("%d/%m/%y")
+            speak(f"Bugünün tarihi {date}")
+        
+        elif 'wikipedia' in text:
             text = text.replace("wikipedia", "")
             wikipedia_search(text)
                   
         elif 'saat kaç' in text:
-            break
+            cur_time = datetime.datetime.now()
+            cur_time = cur_time.strftime("%H:%M:%S")
+            speak(f"Saat {cur_time}")
         
         elif 'ara' in text:
             text = text.replace("ara", "")
             google_search(text)
             speak("İşte bunları buldum", 6)
             
-        elif "hesapla" or "kaç eder" or "kaçtır" in text: 
-            break    
+        elif "hesapla" or "kaç eder" or "kaçtır" in text:
+            api_id = "LYH8JE-XHUWTAJVJX"
+            client = wolframalpha.Client(api_id)
+            indx = text.lower().split().index('calculate')
+            text = text.split()[indx + 1:]
+            res = client.query(' '.join(text))
+            answer = next(res.results).text
+            print("İşlemin sonucu: " + answer)
+            speak("İşlemin sonucu " + answer)
+            
             
         elif 'google aç' in text:
             driver = webdriver.Chrome(r"C:\Program Files (x86)\Google\Chrome\Application\chrome.exe")
@@ -75,12 +96,43 @@ def listen():
             text = text.replace("youtube", "")
             youtube_search(text)
              
+        
         elif 'müzik çal' in text:
-            break
-        elif 'hava' in text:
-            break
+            text = text.replace("youtube", "")
+            youtube_search(text)
+            
+        
+        elif 'hava durumu' in text:
+            text_city = text.replace("hava durumu", "")
+            api_key="e2024c81d0ce1686c5b70152fdd01b9d"
+            base_url="https://api.openweathermap.org/data/2.5/weather?"
+            complete_url = base_url + "appid="+api_key+"&q="+ text_city +"&lang=tr"+"&units=metric"
+            response = requests.get(complete_url)
+            x=response.json()
+            if x["cod"]!="404":
+                y=x["main"]
+                cur_temp = y["temp"]
+                cur_hum = y["humidity"]
+                z = x["weather"]
+                weather_des = z[0]["description"]
+                w_f1 = f"Bugün {text_city} ilinde hava "
+                w_f2 = f"{cur_temp} derece nem oranı "
+                w_f3 = f"{cur_hum} ve hava {weather_des}"
+                weather_forecast = w_f1 + w_f2 + w_f3
+                speak(weather_forecast)
+                print(weather_forecast)
+        
         elif 'sesi aç' or 'sesi yükselt' or 'sesi arttır' in text:
             break
+        
+        elif "sesi azalt" or "sesi düşür" in text:
+            break
+        
+        elif "haberler" in text:
+            webdriver.Chrome("https://www.bbc.com/turkce")
+            speak("BBC Türkçeden haberler, iyi okumalar")
+            time.sleep(6)
+        
         else:
            speak("Üzgünüm, isteğinizi yerine getiremiyorum", 2)
 
